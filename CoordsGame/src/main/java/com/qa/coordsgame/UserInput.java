da55e9d812e8568daab8411f811d6c218dcecd46
Gurinder Singh <graphiczz8@gmail.com>
2022-10-21 11:45:30 +0100

    UserClass firstdraft

--- CoordsGame/src/main/java/com/qa/coordsgame/UserInput.java
+++ CoordsGame/src/main/java/com/qa/coordsgame/UserInput.java
@@ -1,5 +1,42 @@
 package com.qa.coordsgame;
 
+import java.util.Scanner;
+
 public class UserInput {
+	Scanner scan = new Scanner(System.in);
+	int intinput;
+	String stringinput;
+
+	public int readInt() {
+		System.out.println("Please enter a number:");
+		while (true) {
+
+			if (scan.hasNextInt()) {
+				intinput = scan.nextInt();
+				return intinput;
+			} else {
+				System.out.println("Invalid input");
+				scan.next();
+			}
+		}
+
+	}
+
+	public String readString() {
+		System.out.println("Please enter a string:");
+		stringinput = scan.nextLine();
+		while (true) {
+
+			if (stringinput.equals("w") || stringinput.equals("a") || stringinput.equals("s")
+					|| stringinput.equals("d")) {
+				return stringinput;
+			} else {
+				System.out.println("Invalid input");
+				stringinput = scan.nextLine();
+			}
+
+		}
+
+	}
 
 }
