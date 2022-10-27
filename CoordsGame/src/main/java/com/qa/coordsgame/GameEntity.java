3bcd2226395fd97b26d4302424a60a5db90ebbac
davidfordjour <davidfordjour98@hotmail.com>
2022-10-27 17:01:54 +0100

    About to add getPosition()

---
+++ CoordsGame/src/main/java/com/qa/coordsgame/GameEntity.java
@@ -0,0 +1,18 @@
+package com.qa.coordsgame;
+
+public class GameEntity {
+	private int positionX, positionY;
+
+	public GameEntity(int positionX, int positionY) {
+		super();
+		this.positionX = positionX;
+		this.positionY = positionY;
+	}
+	
+	public int getPosition() {
+		// Array for X and Y position.
+		return 0;
+	}
+	
+	
+}
